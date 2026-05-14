1b6d6db234e294806f5903dec5187ee09b60df58
Ryad Bouderga <ryad.bouderga78@gmail.com>
2026-05-14 21:42:49 +0200

    feat(gauntlet): hypothesis - champ optionnel prepare_features

--- 02_validation/gauntlet/tests/test_hypothesis.py
+++ 02_validation/gauntlet/tests/test_hypothesis.py
@@ -41,3 +41,31 @@ def test_hypothesis_build_variant_non_callable_leve_erreur():
             name="bad", description="", instrument="MNQ", timeframe="5min",
             build_variant="pas une fonction", param_grid=[{"timeout_bars": 12}],
         )
+
+
+def test_hypothesis_prepare_features_default_none():
+    h = Hypothesis(
+        name="dummy", description="", instrument="MNQ", timeframe="5min",
+        build_variant=_dummy_build_variant, param_grid=[{"timeout_bars": 12}],
+    )
+    assert h.prepare_features is None
+
+
+def test_hypothesis_prepare_features_accepts_callable():
+    def _prep(df):
+        return df
+    h = Hypothesis(
+        name="dummy", description="", instrument="MNQ", timeframe="5min",
+        build_variant=_dummy_build_variant, param_grid=[{"timeout_bars": 12}],
+        prepare_features=_prep,
+    )
+    assert h.prepare_features is _prep
+
+
+def test_hypothesis_prepare_features_non_callable_leve_erreur():
+    with pytest.raises(TypeError):
+        Hypothesis(
+            name="bad", description="", instrument="MNQ", timeframe="5min",
+            build_variant=_dummy_build_variant, param_grid=[{"timeout_bars": 12}],
+            prepare_features="pas une fonction",
+        )
